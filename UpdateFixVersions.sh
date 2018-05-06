626c99e7744a6a79b9298bca097802521c877498
Volodymyr <vovochkastelmashchuk@gmail.com>
2018-05-06 17:17:27 +0300

    Create UpdateFixVersions.sh

---
+++ UpdateFixVersions.sh
@@ -0,0 +1,10 @@
+file="releaseNote.txt"
+
+if [ -f "$file" ]
+then
+ for key in $(cat releaseNote.txt)
+  do
+      curl -D- -u $JIRA_USERNAME:$JIRA_PASSWORD -X PUT --data "{\"update\":{\"fixVersions\":[{\"set\":[{\"name\":\"${BUILD_NUMBER}\"}]}]}}" -H "Content-Type:application/json" ${JIRA_URL}rest/api/2/issue/${key}
+else
+  echo "$file not found."
+fi
